464a20fd821022a89f336f128744c42170fa8dc1
Ole Loigu <ole.loigu@avalanchelabs.ee>
2020-10-03 20:42:46 +0300

    commit

---
+++ exercises/X01/index.js
@@ -0,0 +1,92 @@
+var people = [
+  {
+    first_name: 'Clifford',
+    last_name: 'Hasloch',
+    gender: 'Male',
+    age: 28,
+    isWearingMask: false,
+  },
+  {
+    first_name: 'Geralda',
+    last_name: 'Soldan',
+    gender: 'Female',
+    age: 19,
+    isWearingMask: false,
+  },
+  {
+    first_name: 'Ianthe',
+    last_name: 'Collidge',
+    gender: 'Female',
+    age: 24,
+    isWearingMask: false,
+  },
+  {
+    first_name: 'Radcliffe',
+    last_name: 'Revill',
+    gender: 'Male',
+    age: 21,
+    isWearingMask: true,
+  },
+  {
+    first_name: 'Chane',
+    last_name: 'Hearle',
+    gender: 'Male',
+    age: 15,
+    isWearingMask: true,
+  },
+  {
+    first_name: 'Yuma',
+    last_name: 'Borsi',
+    gender: 'Male',
+    age: 20,
+    isWearingMask: false,
+  },
+  {
+    first_name: 'Pinchas',
+    last_name: 'Duddin',
+    gender: 'Male',
+    age: 19,
+    isWearingMask: true,
+  },
+  {
+    first_name: 'Constantia',
+    last_name: 'Enderle',
+    gender: 'Female',
+    age: 22,
+    isWearingMask: true,
+  },
+  {
+    first_name: 'Kerwinn',
+    last_name: 'Gorhardt',
+    gender: 'Male',
+    age: 18,
+    isWearingMask: true,
+  },
+  {
+    first_name: 'Ludovico',
+    last_name: 'Orenzel;',
+    gender: 'Male',
+    age: 31,
+    isWearingMask: true,
+  },
+];
+console.log(people);
+
+/**
+ * This is an array of objects of people who would like to go to an event.
+ * Make an array of people attending an event, both first name and last name
+ * and print their last and first name into console.
+ *
+ * Extra:
+ * #1 The event they would like to go to is an 21+ event.
+ * So everyone aged less than 21 cannot get inside.
+ *
+ * #2 Print out all males or females that went to the 21+ event.
+ *
+ * #3 For current safety reasons we must randomly give every person a 50% chance of getting in.
+ *
+ * #4 If a persons first or last name starts with C, G, F, L, X or W they sadly cannot come in to the event.
+ * Write the alert/console message for which letter they cannot be admitted for.
+ *
+ *
+ *  */
